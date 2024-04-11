a003f15ded731faa897ef29aff59924e532e2c80
Zulfiqar Ahmed <zulfiqar.ahmed@e-go-mobile.com>
2024-04-11 16:55:31 +0200

    feat: use quotable api for quotes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,44 @@
-import http from "node:http";
+import dotenv from "dotenv";
 import fs from "node:fs";
+import http from "node:http";
 import path from "node:path";
-import { dir } from "node:console";
 
-const PORT = 9001;
+dotenv.config();
 
-const server = http.createServer((req, res) => {
-    const rs = fs.createReadStream(path.resolve(__dirname, "views/index.html"));
+async function main() {
+    const evergreenQuote = process.env.EVERGREEN_QUOTE;
+    if (!evergreenQuote) {
+        throw new Error("Evergreen Quote is undefined!");
+    }
 
-    rs.on("close", () => {
+    const port = process.env.PORT;
+    if (!port) {
+        throw new Error("Port is undefined!");
+    }
+
+    const quotableApiUrl = process.env.QUOTABLE_API_URL;
+    if (!quotableApiUrl) {
+        throw new Error("Url for Quotable API is undefined!");
+    }
+
+    const server = http.createServer(async (req, res) => {
+        const quote = await (await fetch(`${quotableApiUrl}/random`)).json();
+
+        const file = fs.readFileSync(path.resolve(__dirname, "../views/index.html"));
+        const content = file.toString().replace("{{quote}}", quote?.content || evergreenQuote);
+
+        res.writeHead(200);
+        res.write(content);
         res.end();
     });
 
-    rs.pipe(res);
+    server.listen(port, () => {
+        console.log(`Server listening on port: ${port}`);
+    });
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exit(1);
 });
 
-server.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
-});
